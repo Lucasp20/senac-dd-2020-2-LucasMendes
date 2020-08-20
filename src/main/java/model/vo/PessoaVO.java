7321c243fe3607eba34c4692ace8a1f9f7292bf6
Lucas Pereira <Lucas Pereira@Pereira>
2020-08-19 21:04:53 -0300

    Inserindo dados na classe pessoaVO

--- src/main/java/model/vo/PessoaVO.java
+++ src/main/java/model/vo/PessoaVO.java
@@ -2,4 +2,53 @@ package model.vo;
 
 public class PessoaVO {
 
+	private int idUsuario;
+	private String nome;
+	private String dataNascimento;
+	private char sexo;
+	private String cpf;
+	
+	public PessoaVO(int idUsuario, String nome, String dataNascimento, char sexo, String cpf) {
+		super();
+		this.idUsuario = idUsuario;
+		this.nome = nome;
+		this.dataNascimento = dataNascimento;
+		this.sexo = sexo;
+		this.cpf = cpf;
+	}
+	public PessoaVO() {
+		super();
+		
+	}
+	public int getIdUsuario() {
+		return idUsuario;
+	}
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public char getSexo() {
+		return sexo;
+	}
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
 }
